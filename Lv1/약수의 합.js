a724dee54413d30078b909e32fed4fea256d029a
쿼카 <42869930+grin-quokka@users.noreply.github.com>
2021-07-15 09:35:19 +0900

    2021.07.15 solved

---
+++ Lv1/약수의 합.js
@@ -0,0 +1,14 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12928
+// 정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
+
+function solution(n) {
+  let result = 0;
+
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      result += i;
+    }
+  }
+
+  return result;
+}
