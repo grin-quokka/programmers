27c168076881b97ea760795e5d70123dc40b67ba
쿼카 <42869930+grin-quokka@users.noreply.github.com>
2021-07-17 23:34:03 +0900

    2021.07.17 solved

---
+++ Lv1/이상한 문자 만들기.js
@@ -0,0 +1,17 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12930
+// 문자열 s는 한 개 이상의 단어로 구성되어 있습니다. 각 단어는 하나 이상의 공백문자로 구분되어 있습니다.
+// 각 단어의 짝수번째 알파벳은 대문자로, 홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, solution을 완성하세요.
+
+function solution(s) {
+  return s
+    .split(' ')
+    .map((word) =>
+      word
+        .split('')
+        .map((str, index) =>
+          index % 2 === 0 ? str.toUpperCase() : str.toLowerCase()
+        )
+        .join('')
+    )
+    .join(' ')
+}
